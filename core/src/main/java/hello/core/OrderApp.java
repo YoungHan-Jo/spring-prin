7e8ae5a2dc78d62197c04587c02ad3951bf358b5
JYH <verin4494@gmail.com>
2022-01-09 20:24:08 +0900

    prin

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -4,24 +4,34 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class MemberApp {
-
+public class OrderApp {
     public static void main(String[] args) {
+
 //        AppConfig appConfig = new AppConfig();
+//
 //        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        // 수동으로 스프링 빈에서 꺼내오기
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
-        System.out.println("new Member = " + member.getName());
-        System.out.println("find Member = " + findMember.getName());
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice() = " + order.calculatePrice());
+
+
     }
 }
---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        // 단일책임의 원칙 잘 지킨 것/ 주문서비스에서는 할인정책까지 알 필요없음
+        // 할인정책이 바뀌어도 할인정책쪽만 바꾸면 나머지에 자동으로 적용됨
+        
+        return new Order(memberId,itemName,itemPrice, discountPrice);
+    }
+}
