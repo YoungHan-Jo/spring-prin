68f148e3ed0cb8f14fccdb36b487da7b3928b2c4
JYH <verin4494@gmail.com>
2022-01-14 01:23:43 +0900

    commit

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)// 예시를 위해 configuration 애노테이션은 제외
+public class AutoAppConfig {
+
+}
