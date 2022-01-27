6baed10d6e5180f21d264d50efa35f4725e46ca2
JYH <verin4494@gmail.com>
2022-01-28 00:59:29 +0900

    0128

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -33,21 +35,19 @@ public class SingletonWithPrototypeTest1 {
         assertThat(count1).isEqualTo(1);
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
 
     }
 
     //@Scope("singleton") // 디폴트값 생략가능
     static class ClientBean {
-        private final ProtoTypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(ProtoTypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<ProtoTypeBean> protoTypeBeanProvider;
 
         public int logic() {
+            ProtoTypeBean prototypeBean = protoTypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
